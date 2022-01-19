cc0085f1d3f385a9eb556469f1aed7f794f463b6
wooseok0727 <crash2@naver.com>
2022-01-19 22:34:34 +0900

    vue3 webpack  template

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    // 사전에 정의된 전역 변수 설정
+  env: {
+    browser: true,
+    node: true
+  },
+    // 사용 가능한 규칙 세트
+  extends: [
+    // vue
+    // 'plugin:vue/vue3-essential', // Lv1
+    'plugin:vue/vue3-strongly-recommended', // Lv2
+    // 'plugin:vue/vue3-recommended', // Lv3
+    // js
+    'eslint:recommended'
+  ],
+  // 구문 분석할 패키지 설정(Babel, ES6^ => ES5)
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+  rules: {
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'vue/html-closing-bracket-newline': ['error', {
+      'singleline': 'never',
+      'multiline': 'never'
+    }],
+    'vue/html-self-closing': ['error', {
+      'html': {
+        'void': 'always',
+        'normal': 'never',
+        'component': 'always'
+      },
+      'svg': 'always',
+      'math': 'always'
+    }]
+  }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Vue3 템플릿 with Webpack
+
+## Installation
+
+```bash
+# Default.
+$ npx degit woooseok0727/vue3-webpack-template DIRECTORY_NAME
